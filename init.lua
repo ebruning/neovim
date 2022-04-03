e96e0f36f674178041f88e5b46d9dea970ddd872
Ethan Bruning <ethan@bruning.io>
2022-04-03 08:16:33 -0700

    move noswapfile and nobackup to global

--- init.lua
+++ init.lua
@@ -7,12 +7,12 @@ vim.g.fillchars = ' '
 vim.g.noshowmode = true
 vim.g.laststatus = 2
 vim.g.nocompatible = true
+vim.g.noswapfile = true
+vim.g.nobackup = true
 
 local options = vim.opt
 options.relativenumber = true
 options.number = true
-options.noswapfile = true
-options.nobackup = true
 options.expandtab = true
 options.splitbelow = true
 options.splitright = true
