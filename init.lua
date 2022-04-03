b2aa44ff7de3dc67942acaaab03c2bb0bc4c0e25
Ethan Bruning <ethan@bruning.io>
2022-04-03 09:16:31 -0700

    set colorscheme

--- init.lua
+++ init.lua
@@ -9,6 +9,7 @@ vim.g.laststatus = 2
 vim.g.nocompatible = true
 vim.g.noswapfile = true
 vim.g.nobackup = true
+vim.g.mapleader = " " 
 
 local options = vim.opt
 options.relativenumber = true
@@ -17,5 +18,14 @@ options.expandtab = true
 options.splitbelow = true
 options.splitright = true
 options.hidden = true
+options.background = 'dark'
+
+vim.cmd('colorscheme gruvbox-material')
 
 -- Add https://github.com/wbthomason/packer.nvim
+require('plugins') --packer config
+require('nvimtree-config')
+
+-- key bindings
+vim.api.nvim_set_keymap('n', '<Leader>n', ':NvimTreeToggle<CR>', { noremap = true, silent = true })
+
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -4,5 +4,13 @@ vim.cmd [[packadd packer.nvim]]
 return require('packer').startup(function()
   -- Packer can manage itself
   use 'wbthomason/packer.nvim'
+  use 'sainnhe/gruvbox-material'
 
+  use {
+    'kyazdani42/nvim-tree.lua',
+    requires = {
+      'kyazdani42/nvim-web-devicons', -- optional, for file icon
+    },
+    config = function() require'nvim-tree'.setup {} end
+}
 end)
